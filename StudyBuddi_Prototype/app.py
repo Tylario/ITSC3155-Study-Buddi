69c006b1e8973afb371335df80c4c41003eac226
RyanHoover159 <139598109+RyanHoover159@users.noreply.github.com>
2024-04-03 16:52:31 -0400

    signup fix

--- StudyBuddi_Prototype/app.py
+++ StudyBuddi_Prototype/app.py
@@ -50,5 +50,9 @@ def login():
 
     return render_template('login.html')
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
